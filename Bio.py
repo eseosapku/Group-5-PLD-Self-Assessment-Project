c7045802b492c29b8c67c90ede9d23146638337b
eseosapku <eseosapku@gmail.com>
2022-08-08 16:59:01 +0200

    commit

--- Bio.py
+++ Bio.py
@@ -1,3 +1,3 @@
 
-name = input('Welcome to Assessment-Buddy this app is aimed to help students who face difficulties with self-assessment and do not know how to perform precise and accurate evaluation of their work as self-directed learners. \nplease type down your name \n'  )
+name = input('Welcome to Assessment-Buddy this app is aimed to help students who face difficulties with self-assessment and do not know how to perform precise and accurate evaluation of their work as self-directed learners. \nplease type down your name to begin \n'  )
 Subject = input(f' What subject would you like to self assess on today {name} \n' )
--- Questions.py
+++ Questions.py
@@ -1,11 +1,21 @@
-questions = list() #empty list
+from optparse import Values
 
-num = input("How many questions do you want to record") #user tells the range(optional)
+
+questions = dict() #empty dict
+
+num = input("How many questions do you want to record  \n" ) #user tells the range(optional)
 
 #iterating till user's range is reached
 for i in range(int(num)): 
-    n = input("Enter a  question: ")#asking for input of 1 value 
-    questions.append(n)#adding that value to the list
+    n = input("Enter a  question: \n")#asking for input of 1 value
+    p = input("Enter the answer to the question: \n")#asking for input of 1 value
+    questions[n]=p#adding that value to the list
 
-print("Here are your listed questions: ")
-print(*questions, sep='\n')
+
+print("*************Here are your listed questions and Answers****************: ")
+for key, values in questions.items():
+    print(key, " = ", values)
+
+
+
+
