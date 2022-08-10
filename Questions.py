0276633a5f87e4354d9536e13e0e989c5c629cfa
eseosapku <eseosapku@gmail.com>
2022-08-10 14:22:45 +0200

    commit

--- Questions.py
+++ Questions.py
@@ -6,9 +6,11 @@ questions = dict() #empty dict
 Subject = dict()
 
 num = input("How many questions do you want to record  \n" ) #user tells the range(optional)
+dig = int(num) - 1
 
-#iterating till user's range is reached
-for i in range(int(num)): 
+    
+
+for i in range(int(num)):
     n = input("Enter a  question: \n")#asking for input of 1 value
     p = input("Enter the answer to the question: \n")#asking for input of 1 value
     questions[n]=p#adding that value to the list
@@ -16,9 +18,13 @@ for i in range(int(num)):
 
 
 
-print("*************Here are your listed questions and Answers****************")
-for key, values in questions.items():
-    print(f"Que) {key}= \nAns) {values} \n")
+print(" \n \n*************Here are your listed questions and Answers****************")
+for i in range(int(num)):
+        fin = int(num) - int(dig)
+        dig = int(dig) - 1
+        for key, values in questions.items(): 
+            print(f"Que{fin}) {key}= \nAns{fin}) {values} \n")
+            break;
 
 Subject[S]=questions
      
@@ -26,7 +32,7 @@ Subject[S]=questions
 Add_que = input('**********Would you like to add more questions*********** \n SELECT Yes OR No \n')
 if Add_que == 'No':
     for key, values in Subject.items():
-        print(f"***********The Subject: {str.capitalize(key)}**************" )
+        print(f" \n***********The Subject: {str.capitalize(key)}**************" )
         for key, values in questions.items():
            print(f"Que) {key}-- \nAns) {values} \n")
         
