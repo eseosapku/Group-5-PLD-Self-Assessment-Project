39cd3a8f96c6639029ea4b3e73712a2d47e46ced
eseosapku <eseosapku@gmail.com>
2022-08-12 19:48:53 +0200

    commit

--- Questions.py
+++ Questions.py
@@ -2,6 +2,7 @@ from bdb import effective
 from optparse import Values
 from Bio import name
 from Bio import S
+ans = dict()
 questions = dict() #empty dict
 Subject = dict()
 
@@ -13,26 +14,36 @@ dig = int(num) - 1
 for i in range(int(num)):
     n = input("Enter a  question: \n")#asking for input of 1 value
     p = input("Enter the answer to the question: \n")#asking for input of 1 value
+    k = input('Enter the keywords the system would use to check your answer to the question \n')
     questions[n]=p#adding that value to the list
-
+    ans[k]=n
 
 
 
 print(" \n \n*************Here are your listed questions and Answers****************")
-for i in range(int(num)):
-        fin = int(num) - int(dig)
-        dig = int(dig) - 1
-        for key, values in questions.items(): 
-            print(f"Que{fin}) {key}= \nAns{fin}) {values} \n")
-            break;
+fin = 1
+for key, values in questions.items(): 
+    print(f"Que{fin}) {key}= \nAns{fin}) {values} \n")
+    fin += 1
 
 Subject[S]=questions
-     
+   
 
 Add_que = input('**********Would you like to add more questions*********** \n SELECT Yes OR No \n')
-if Add_que == 'No':
+if Add_que == 'No' or 'no':
     for key, values in Subject.items():
         print(f" \n***********The Subject: {str.capitalize(key)}**************" )
         for key, values in questions.items():
            print(f"Que) {key}-- \nAns) {values} \n")
         
+
+print('Let us begin the self assessment')
+
+for key, values in questions.items(): 
+    print(f"Que) {key}= \n")
+    answer = input('Type the answer \n')
+    if answer in ans:
+        print('correct')
+    else:
+        print('Incorrect')
+        print(f'correct answer is {values}')
